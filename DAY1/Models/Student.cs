7069dd9d2743b3aabcbdf8143e5130aaf2a76bfc
ShubhKachhadiyaBacancy <shubh.kachhadiya@bacancy.com>
2025-03-06 12:46:14 +0530

    Add project files.

---
+++ DAY1/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DAY1.Models
+{
+    public class Student
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+        [Required]
+        [Column(TypeName ="decimal(10,2)")]
+        public float Marks { get; set; }
+
+    }
+}
