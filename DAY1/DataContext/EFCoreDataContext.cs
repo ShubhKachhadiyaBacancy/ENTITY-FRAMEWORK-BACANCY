05dc5c90b993aa92aa7d14b022d2b16e29cf4fe5
ShubhKachhadiyaBacancy <shubh.kachhadiya@bacancy.com>
2025-03-06 12:58:18 +0530

    Implemented Standard EF Core DI Registration setup.

---
+++ DAY1/DataContext/EFCoreDataContext.cs
@@ -0,0 +1,14 @@
+﻿using DAY1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAY1.DataContext
+{
+    public class EFCoreDataContext:DbContext
+    {
+        public EFCoreDataContext(DbContextOptions<EFCoreDataContext> options): base(options)
+        {
+        }
+
+        public DbSet<Student> students { get; set; }
+    }
+}
