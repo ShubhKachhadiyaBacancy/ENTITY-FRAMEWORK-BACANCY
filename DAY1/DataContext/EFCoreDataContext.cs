17b4058e8b049e106ef7845d468e5e692bbb8e7c
ShubhKachhadiyaBacancy <shubh.kachhadiya@bacancy.com>
2025-03-06 14:24:59 +0530

    Added Student table using OnConfiguration

---
+++ DAY1/DataContext/EFCoreDataContext.cs
@@ -0,0 +1,18 @@
+﻿using DAY1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAY1.DataContext
+{
+    public class EFCoreDataContext : DbContext  
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=LAPTOP-RQLUK45G\\SQLEXPRESS;Database=Student_Details;Trusted_Connection=True; TrustServerCertificate=True;");
+            }
+        }
+
+        DbSet<Student> studentsUsingOnConfiguring { get; set; }
+    }
+}
